9ba2c0f20c94ecab4c4e2e8dac063796e102863e
Cristina Irimes <92781129+cristinairimes@users.noreply.github.com>
2023-03-17 18:48:59 +0200

    Delete Order Management System/pt2022_30224_irimes_cristina_assignment_3-master directory

--- Order Management System/pt2022_30224_irimes_cristina_assignment_3-master/src/main/resources/init.sql
+++
@@ -1,8 +0,0 @@
-DROP DATABASE `assig3`;
-CREATE SCHEMA `assig3`;
-CREATE TABLE `assig3`, `client`(
-    `id` INT NOT NULL AUTO_INCREMENT ,
-    `name` VARCHAR(64) NOT NULL,
-    `address` VARCHAR(64) NOT NULL,
-    PRIMARY KEY(`id`)
-    );
